ea9a2a997c365ecb8cc5b1e68510cc43010036f0
Adrian <56790744+zoladrian@users.noreply.github.com>
2023-10-13 21:41:46 +0200

    Create view and viewodel for parser

--- Autocomp.Nmea.PrismApp/Modules/Autocomp.Nmea.PrismApp.Modules.ModuleName/ViewModels/ParserViewModel.cs
+++ Autocomp.Nmea.PrismApp/Modules/Autocomp.Nmea.PrismApp.Modules.ModuleName/ViewModels/ParserViewModel.cs
@@ -1,25 +1,77 @@
-﻿using Autocomp.Nmea.PrismApp.Core.Mvvm;
-using Prism.Regions;
+﻿using Autocomp.Nmea.Common;
+using Autocomp.Nmea.Models;
+using Autocomp.Nmea.Parsers;
+using Autocomp.Nmea.Parsers.Interfaces;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
 
 namespace Autocomp.Nmea.PrismApp.Modules.ModuleName.ViewModels
 {
-    public class ParserViewModel : RegionViewModelBase
+    public class ParserViewModel : BindableBase
     {
         private string _message;
+        private string _parsedData;
+        private string _nmeaInput;
+
         public string Message
         {
             get { return _message; }
             set { SetProperty(ref _message, value); }
         }
 
-        public ParserViewModel(IRegionManager regionManager) :
-            base(regionManager)
+        public string ParsedData
         {
+            get { return _parsedData; }
+            set { SetProperty(ref _parsedData, value); }
         }
 
-        public override void OnNavigatedTo(NavigationContext navigationContext)
+        public string NmeaInput
         {
-            //do something
+            get { return _nmeaInput; }
+            set { SetProperty(ref _nmeaInput, value); }
+        }
+
+        public ICommand ParseCommand { get; private set; }
+
+        private readonly INmeaParser<GLLMessageData> _gllParser;
+        private readonly INmeaParser<MWVMessageData> _mwvParser;
+
+        public ParserViewModel(INmeaParser<GLLMessageData> gllParser, INmeaParser<MWVMessageData> mwvParser)
+        {
+            _gllParser = gllParser;
+            _mwvParser = mwvParser;
+            ParseCommand = new DelegateCommand(ParseNmea);
+        }
+
+        private void ParseNmea()
+        {
+            if (string.IsNullOrEmpty(NmeaInput)) return;
+
+            NmeaMessage nmeaMessage = new NmeaMessage(NmeaInput);
+
+            if (_gllParser.CanParse(nmeaMessage.Header))
+            {
+                var result = _gllParser.Parse(nmeaMessage);
+                if (result.Success)
+                    ParsedData = result.Data.ToString();
+                else if (result.ErrorMessage is not null)
+                    ParsedData = result.ErrorMessage;
+            }
+            else if (_mwvParser.CanParse(nmeaMessage.Header))
+            {
+
+                var result = _mwvParser.Parse(nmeaMessage);
+                if (result.Success)
+                    ParsedData = result.Data.ToString();
+                else if (result.ErrorMessage is not null)
+                    ParsedData = result.ErrorMessage;
+            }
+            else
+            {
+                ParsedData = "Unknown header";
+            }
         }
     }
 }
